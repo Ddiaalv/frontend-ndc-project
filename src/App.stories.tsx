a482ddc60f22f873ab22f4c70d242f59e18d6d4c
David <ddiaalv@gmail.com>
2020-06-17 19:06:30 +0100

    modified app with notebook style

--- src/App.stories.tsx
+++ src/App.stories.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react';
 import { withA11y } from '@storybook/addon-a11y';
+import App from './App';
 
 export default {
-    title: 'button',
-    decorators: [withA11y],
+  title: 'Nodeca',
+  decorators: [withA11y],
 };
 
-export const accessible = () => <button>Accessible button</button>;
-
-export const inaccessible = () => (
-    <button style={{ backgroundColor: 'red', color: 'darkRed' }}>
-        Inaccessible button
-    </button>
-);
+export const defaultView = () => <App />;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import bg from '../src/assets/bg.jpg';
+import bgNoteBook from '../src/assets/bg-notebook.jpg';
+
+const bgStyles = {
+  app: {
+    background: `url(${bg})`,
+  },
+  notebook: {
+    background: `url(${bgNoteBook})`,
+  },
+};
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={bgStyles.app}>
+      <div id="notebook" style={bgStyles.notebook}>
+        <div id="notebook-paper">
+          <div className="paper-page paper-page-one" />
+          <div className="paper-page paper-page-two" />
+          <div className="paper-page paper-page-three" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
