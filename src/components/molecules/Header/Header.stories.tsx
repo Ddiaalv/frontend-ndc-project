3970fdc4cdacc73ad076cd6e11dc58355e72bd55
David <ddiaalv@gmail.com>
2020-06-17 21:38:13 +0100

    Header added

---
+++ src/components/molecules/Header/Header.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Header } from './Header';
+import { withA11y } from '@storybook/addon-a11y';
+import { BrowserRouter } from 'react-router-dom';
+
+export default {
+  title: 'Header',
+  decorators: [withA11y, (storyFn: any) => <BrowserRouter>{storyFn()}</BrowserRouter>],
+};
+
+export const withText = () => <Header />;
