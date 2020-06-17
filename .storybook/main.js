92db89cddcdc06b72a14c28c671615147768daab
David <ddiaalv@gmail.com>
2020-06-17 18:41:34 +0100

    Fix minimist and install addons for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,18 @@
 module.exports = {
-    addons: [
-        '@storybook/addon-actions/register',
-        '@storybook/addon-knobs/register',
-        '@storybook/addon-a11y/register',
-    ],
-    stories: ['../src/**/*.stories.tsx'],
-    webpackFinal: async (config) => {
-        config.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            loader: require.resolve('react-docgen-typescript-loader'),
-        });
-        config.resolve.extensions.push('.ts', '.tsx');
-        return config;
-    },
+  addons: [
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-storysource',
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-viewport/register',
+  ],
+  stories: ['../src/**/*.stories.tsx'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('react-docgen-typescript-loader'),
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
 };
